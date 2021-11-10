475ff804aad1a3530047f495237867d725a0119f
Apeofspace <sintrael@gmail.com>
2021-11-10 13:06:24 +0300

    Despagettify code
    Changed buffer lists into one dictionary of buffers

--- main2.py
+++ main2.py
@@ -1,4 +1,3 @@
-import threading
 import multiprocessing
 import tkinter as tk
 from tkinter import ttk
@@ -13,7 +12,7 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import numpy as np
 import math
-from queue import Empty, Full
+from queue import Empty
 
 
 class MainWindow(tk.Frame):
@@ -27,34 +26,34 @@ class MainWindow(tk.Frame):
         self.hertz = hertz
         self.reader_process = None
         self.mode = mode
+        self.buffer_size = 15000
+        self.show_on_plot = 2000
+        fields = ['Time COM',
+                  'COM',
+                  'Time OBJ',
+                  'OBJ',
+                  'Duty',
+                  'Dir']
+        self.buffers = {name: [] for name in fields}
         # PLOT
         self.fig, self.ax = plt.subplots(figsize=(10, 5), tight_layout=True)
         self.canvas = FigureCanvasTkAgg(self.fig, master=self)
         self.toolbar = NavigationToolbar2Tk(self.canvas, self)
         self.toolbar.update()
         self.toolbar.pack(side='top')
-        self.x = np.arange(0, 2 * np.pi, 0.01)
         plt.grid(b=True, which='major', axis='both')
         self.ax2 = self.ax.twinx()
         self.ax2.format_coord = self.make_format(self.ax, self.ax2)
-        self.line_duty, = self.ax2.plot(self.x, 2000 * np.sin(self.x) + np.pi, label='Коэффициент заполнения',
+        self.line_duty, = self.ax2.plot(0, 0, label='Коэффициент заполнения',
                                         color='green', linewidth=0.5)
-        self.line_dir, = self.ax2.plot(self.x, 2000 * np.sin(self.x) + np.pi, label='Направление',
+        self.line_dir, = self.ax2.plot(0, 0, label='Направление',
                                        color='red', linewidth=0.5)
         # , linewidth = 0.5, marker = "o", markersize = 0.5
-        self.line1, = self.ax.plot(self.x, 2000 * np.sin(self.x), label='Управляющий сигнал')
-        self.line2, = self.ax.plot(self.x, 2000 * np.sin(self.x) + np.pi, label='Значение с потенциометра')
+        self.line1, = self.ax.plot(0, 0, label='Управляющий сигнал')
+        self.line2, = self.ax.plot(0, 0, label='Значение с потенциометра')
         self.fig.legend()
         self.ax.set_ylim(0, 4100)
         self.ax2.set_ylim(0, 4100)
-        self.buffer_size = 15000
-        self.show_on_plot = 2000
-        self.buffer_x_com = []
-        self.buffer_y_com = []
-        self.buffer_x_obj = []
-        self.buffer_y_obj = []
-        self.buffer_duty = []
-        self.buffer_dir = []
         self.canvas.get_tk_widget().pack(side='top', fill='both', expand=True)
         self.ani = FuncAnimation(self.fig, self.animate, interval=16, blit=False)
         plt.xlabel("[мс]")
@@ -116,6 +115,7 @@ class MainWindow(tk.Frame):
             # convert back to data coords with respect to ax
             ax_coord = inv.transform(display_coord)
             return ("Координата: {:.0f},   коэф. заполнения: {:.0f},   время: {:.2f}".format(ax_coord[1], y, x))
+
         return format_coord
 
     def button_press(self):
@@ -127,29 +127,20 @@ class MainWindow(tk.Frame):
     def animate(self, i):
         while self.main_queue.qsize():
             try:
-                # ахтунг! спагетти код
                 buf = self.main_queue.get()
-                self.buffer_x_com.append(buf["Time COM"])
-                self.buffer_y_com.append(buf["COM"])
-                self.buffer_x_obj.append(buf["Time OBJ"])
-                self.buffer_y_obj.append(buf["OBJ"])
-                self.buffer_duty.append(buf["Duty"])
-                self.buffer_dir.append(buf["Dir"])
-                if len(self.buffer_x_com) > self.buffer_size:
-                    self.buffer_x_com.pop(0)
-                    self.buffer_y_com.pop(0)
-                    self.buffer_x_obj.pop(0)
-                    self.buffer_y_obj.pop(0)
-                    self.buffer_duty.pop(0)
-                    self.buffer_dir.pop(0)
-                if len(self.buffer_x_com) > self.show_on_plot:
-                    self.ax.set_xlim(self.buffer_x_com[-self.show_on_plot], self.buffer_x_com[-1])
-                elif len(self.buffer_x_com) > 1:
-                    self.ax.set_xlim(self.buffer_x_com[0], self.buffer_x_com[-1])
-                self.line1.set_data(self.buffer_x_com, self.buffer_y_com)
-                self.line2.set_data(self.buffer_x_obj, self.buffer_y_obj)
-                self.line_duty.set_data(self.buffer_x_com, self.buffer_duty)
-                self.line_dir.set_data(self.buffer_x_com, self.buffer_dir)
+                for key in self.buffers.keys():
+                    self.buffers[key].append(buf[key])
+                if len(self.buffers["Time COM"]) > self.buffer_size:
+                    for value in self.buffers.values():
+                        value.pop()
+                if len(self.buffers["Time COM"]) > self.show_on_plot:
+                    self.ax.set_xlim(self.buffers["Time COM"][-self.show_on_plot], self.buffers["Time COM"][-1])
+                elif len(self.buffers["Time COM"]) > 1:
+                    self.ax.set_xlim(self.buffers["Time COM"][0], self.buffers["Time COM"][-1])
+                self.line1.set_data(self.buffers["Time COM"], self.buffers["COM"])
+                self.line2.set_data(self.buffers["Time OBJ"], self.buffers["OBJ"])
+                self.line_duty.set_data(self.buffers["Time COM"], self.buffers["Duty"])
+                self.line_dir.set_data(self.buffers["Time COM"], self.buffers["Dir"])
             except Empty:
                 print("empty que =(")
 
@@ -163,25 +154,18 @@ class MainWindow(tk.Frame):
     def connect(self):
         p = re.search("COM[0-9]+", self.COMbobox.get())
         if p:
-            self.buffer_x_com = []
-            self.buffer_y_com = []
-            self.buffer_x_obj = []
-            self.buffer_y_obj = []
-            self.buffer_duty = []
-            self.buffer_dir = []
+            for key in self.buffers.keys():
+                self.buffers[key] = []
             com_port = p[0]
             self.label_status.configure(text='Подключение...')
             self.lock.acquire()
             stop_flag.value = 0
-            self.processes = []
             print("process starting...")
             self.reader_process = multiprocessing.Process(target=read_process, args=(
                 self.stop_flag, self.connected_flag, com_port, self.lock, self.main_queue, self.msg_queue, self.hertz,
                 self.mode), daemon=True)
             self.reader_process.start()
             self.check_msg()
-            # k = 0
-            # told = time.time()
             self.connected_flag.value = 1
             self.lock.release()
             i = 0
@@ -194,12 +178,12 @@ class MainWindow(tk.Frame):
                     self.disconnect()
             if self.msg_queue.empty() is False:
                 msg = self.msg_queue.get()
-                print("message in connect: {}".format(msg))
+                print(f"Connected to : {msg}")
                 if msg == serial.SerialException:
                     self.label_status.configure(text=msg)
                     self.disconnect()
                 else:
-                    self.label_status.configure(text='Подключено к {}'.format(msg))
+                    self.label_status.configure(text=f'Подключено к {msg}')
                     self.button_connect.configure(text="Отключиться")
 
     def disconnect(self):
@@ -232,6 +216,7 @@ def read_process(stop_flag, connected_flag, com_port, lock, queue, msg_queue, he
     k = 0
     first_time = True
     signal = 0
+    fields = ["Time COM", "Time OBJ", "COM", "OBJ", "Duty", "Dir"]
     try:
         ser.baudrate = 115200
         ser.port = com_port
@@ -239,7 +224,7 @@ def read_process(stop_flag, connected_flag, com_port, lock, queue, msg_queue, he
         msg_queue.put(ser.portstr)
         with open("Data_{}.csv".format(datetime.datetime.now().strftime("%Y_%m_%d-%H%M%S")), 'w',
                   newline='') as csv_file:
-            csv_writer = csv.DictWriter(csv_file, fieldnames=["Time COM", "Time OBJ", "COM", "OBJ", "Duty", "Dir"])
+            csv_writer = csv.DictWriter(csv_file, fieldnames=fields)
             csv_writer.writeheader()
             while True:
                 lock.acquire()
